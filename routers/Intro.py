9a996424d970e7f8ca86c1018cbab53c7a95f5a3
Harrison Doyle <doyle266@gmail.com>
2026-06-06 21:26:22 -0500

    removed /

--- routers/Intro.py
+++ routers/Intro.py
@@ -4,9 +4,9 @@ from db.database import is_database_ready
 
 router = APIRouter(tags=["intro"])
 
-@router.get("/")
-def root():
-    return {"message": "Hello, World!"}
+#@router.get("/")
+#def root():
+    #return {"message": "Hello, World!"}
 
 @router.get("/ready")
 def ready():
